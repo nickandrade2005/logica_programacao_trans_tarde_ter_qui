9c33cb0a8ff536efd6a38100aa199fd6e39a4ba9
NICOLAS RAFAEL DA HORA ANDRADE <nicolas.rhandrade@senacsp.edu.br>
2025-01-14 15:25:39 -0300

    first commit

---
+++ ex02/Program.cs
@@ -0,0 +1,2 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("ola sou nick estou aprendendo programaçao");
---
+++ ex03/Program.cs
@@ -0,0 +1,2 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("ola estou estudando no senac largo treze");
